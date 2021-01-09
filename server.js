6d1ba9df3e3ef260776c92d6c048e907c71e686b
repl.it user <replituser@example.com>
2021-01-09 12:08:44 +0000

    commit after finishing the challenges

--- server.js
+++ server.js
@@ -8,21 +8,29 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+const bcrypt = require('bcrypt')
+
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log('hash:', hash)
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log('res:', res)
+  })
+})
 
 //END_ASYNC
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log('hash:', hash)
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log('result:', result)
 
 //END_SYNC
-
-
-
 
 
 
